4770e078057925ec477ef47a2cd455791141bb72
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2025-01-12 18:21:35 +0100

    imported new input system

--- Assets/Fighter.cs
+++ Assets/Fighter.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using static UnityEngine.InputSystem.InputAction;
 public class Fighter : MonoBehaviour
 {
     public float moveSpeed = 5f;   // Speed of movement
     public float jumpForce = 7f;   // Force applied when jumping
     private bool isGrounded;       // Check if player is on the ground
     private Rigidbody2D rb;        // Rigidbody2D component
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +18,17 @@ public class Fighter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        //Move();
         Jump();
     }
 
-    void Move()
+    public void Move(CallbackContext callbackContext)
     {
-        float inputX = Input.GetAxis("Horizontal"); // Get horizontal input (-1 to 1)
+        float inputX = callbackContext.ReadValue<float>();
         Vector3 move = new Vector3(inputX, 0, 0) * moveSpeed * Time.deltaTime;
         //transform.Translate(move); // Move the player
         rb.velocity = new Vector2(-move.x, rb.velocity.y);
+        print(rb.velocity);
     }
 
     void Jump()
