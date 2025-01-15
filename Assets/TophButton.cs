55d9f8d5c8da6db8b2bcb8b74758c5b58d4479f1
VM Marco <marcoxativa@gmail.com>
2025-01-15 18:57:07 +0100

    select screen

---
+++ Assets/TophButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TophButton : MonoBehaviour
+{
+    public void PlayAsToph()
+    {
+        SceneManager.LoadScene("TophScene");
+    }
+
+
+}
