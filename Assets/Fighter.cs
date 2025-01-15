dc447f929346855a5fed1127dcd702c0680ae481
VM Marco <marcoxativa@gmail.com>
2025-01-15 20:40:35 +0100

    bug fixes

--- Assets/Fighter.cs
+++ Assets/Fighter.cs
@@ -23,10 +23,15 @@ public class Fighter : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         //Move();
-       // animator.SetFloat("input_x", Mathf.Abs( rb.velocity.x ));
+        // animator.SetFloat("input_x", Mathf.Abs( rb.velocity.x ));
+
+        animator.ResetTrigger("w_kick");
+        animator.ResetTrigger("s_kick");
+        animator.ResetTrigger("s_punch");
+        animator.ResetTrigger("w_punch");
     }
 
     public void Move(Vector2 input)
@@ -36,7 +41,7 @@ public class Fighter : MonoBehaviour
        
 
         Vector3 move = new Vector3(inputX, 0, 0) * moveSpeed * Time.deltaTime;
-        if (playerIndex == 0) { move.x *= -1; }
+        if (playerIndex == 1) { move.x *= -1; }
         //transform.Translate(move); // Move the player
         //rb.velocity = new Vector2(-move.x, rb.velocity.y);
         //if (move.x < 0)
@@ -65,8 +70,37 @@ public class Fighter : MonoBehaviour
         AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
         if (stateInfo.IsName("Idle") || stateInfo.IsName("walk") || stateInfo.IsName("walk_0"))
         {
+            print("Fighter is performing " + atk_name);
+
             animator.SetTrigger(atk_name);
             voiceLines?.PlayRandomClip();
+            if (atk_name == "s_punch")
+            {
+                animator.ResetTrigger("s_kick");
+                animator.ResetTrigger("w_kick");
+                animator.ResetTrigger("w_punch");
+            }
+            else if (atk_name == "s_kick")
+            {
+                animator.ResetTrigger("s_punch");
+                animator.ResetTrigger("w_kick");
+                animator.ResetTrigger("w_punch");
+
+            }
+            else if (atk_name == "w_kick")
+            {
+                animator.ResetTrigger("s_punch");
+                animator.ResetTrigger("s_kick");
+                animator.ResetTrigger("w_punch");
+
+            }
+            else if (atk_name == "w_punch")
+            {
+                animator.ResetTrigger("w_kick");
+                animator.ResetTrigger("s_kick");
+                animator.ResetTrigger("s_punch");
+
+            }
         }
     }
 
